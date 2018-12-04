5ff3831ba6bdccd235c611264303ab544b934c8f
Andrew Chalkley <andrew@chalkley.org>
2018-12-03 22:48:00 -0800

    Created blocks. Created test.

--- main.ts
+++ main.ts
@@ -3,27 +3,40 @@
  */
 
 enum HT16K33_I2C_ADDRESSES {
+    //% block="0x70 (Default)"
     ADD_0x70 = 0x70,
+    //% block="0x71"
     ADD_0x71 = 0x71,
+    //% block="0x72"
     ADD_0x72 = 0x72,
+    //% block="0x73"
     ADD_0x73 = 0x73,
+    //% block="0x74"
     ADD_0x74 = 0x74,
+    //% block="0x75"
     ADD_0x75 = 0x75,
+    //% block="0x76"
     ADD_0x76 = 0x76,
+    //% block="0x77"
     ADD_0x77 = 0x77,
 }
 
 enum HT16K33_COMMANDS {
-    DEFAULT_ADDRESS = HT16K33_I2C_ADDRESSES.ADD_0x70,
     TURN_OSCILLATOR_ON = 0x21,
     TURN_DISPLAY_ON = 0x81,
     SET_BRIGHTNESS = 0xE0
 }
 
 enum HT16K33_CONSTANTS {
+    DEFAULT_ADDRESS = HT16K33_I2C_ADDRESSES.ADD_0x70,
     MAX_BRIGHTNESS = 15,
     MAX_BLINK_RATE = 3
 }
+
+/**
+ * HT16K33 block
+ */
+//% weight=100 color=#00a7e9 icon="\uf26c" block="HT16K33"
 namespace ht16k33 {
     let matrixAddress = 0;
 
@@ -45,8 +58,8 @@ namespace ht16k33 {
         )
     }
 
-
-    export function render(bitmap: Array<number>) {
+    //% blockId="HT16K33_RENDER_BITMAP" block="render bitmap %bitmap"
+    export function render(bitmap: number[]) {
         const formattedBitmap = formatBimap(bitmap)
         let buff = pins.createBufferFromArray(formattedBitmap);
         pins.i2cWriteBuffer(matrixAddress, buff, false);
@@ -73,7 +86,7 @@ namespace ht16k33 {
         sendCommand(HT16K33_COMMANDS.TURN_DISPLAY_ON)
         setBrightness(15);
     }
-
+    //% blockId="HT16K33_SET_ADDRESS" block="set address %address"
     export function setAddress(address: HT16K33_I2C_ADDRESSES) {
         if (matrixAddress != address) {
             matrixAddress = address;
@@ -81,10 +94,13 @@ namespace ht16k33 {
         }
     }
 
+    //% blockId="HT16K33_SET_BRIGHTNESS" block="set brightness %brightness"
+    //% brightness.min=0 brightness.max=15
     export function setBrightness(brightness: number) {
         sendCommand(HT16K33_COMMANDS.SET_BRIGHTNESS | brightness & HT16K33_CONSTANTS.MAX_BRIGHTNESS);
     }
-
+    //% blockId="HT16K33_SET_BLINK_RATE" block="set blink rate %rate"
+    //% rate.min=0 rate.max=3
     export function setBlinkRate(rate: number) {
         sendCommand(HT16K33_COMMANDS.TURN_DISPLAY_ON | (rate & HT16K33_CONSTANTS.MAX_BLINK_RATE) << 1);
     }
--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
-// tests go here; this will not be compiled when this package is used as a library
+let frown: number[] = []
+let meh: number[] = []
+let smile: number[] = []
+ht16k33.setAddress(HT16K33_I2C_ADDRESSES.ADD_0x70)
+smile = [60, 66, 165, 129, 165, 153, 66, 60]
+meh = [60, 66, 165, 129, 189, 129, 66, 60]
+frown = [60, 66, 165, 129, 153, 165, 66, 60]
+forever(function () {
+    ht16k33.setBrightness(0)
+    ht16k33.render(frown)
+    pause(1000)
+    ht16k33.setBrightness(7)
+    ht16k33.render(meh)
+    pause(1000)
+    ht16k33.setBrightness(15)
+    ht16k33.render(smile)
+    pause(1000)
+})
